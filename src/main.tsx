5774f43ae00875e8cf4d8a8f25afe4f0413f84e7
Cristhian <alfredogarzon1234@hotmail.com>
2024-11-05 08:55:12 -0500

    [ADD] react Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AppProvider } from './app/core/state/AppContext';
-import HomeContainer from './app/container/HomeContainer';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/routes/indext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProvider>
-      <HomeContainer />
-    </AppProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
