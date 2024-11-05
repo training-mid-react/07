0bcfe2a7714fbccb44d4fce20651db56f576de82
Henry P Salazar <henry.salazar@sofka.com.co>
2024-11-05 09:35:06 -0500

    add current player play paragraph

--- src/app/ui/layouts/principalPage.tsx
+++ src/app/ui/layouts/principalPage.tsx
@@ -4,11 +4,12 @@ import './../../ui/styles/index.scss';
 
 export const PrincipalPage = () => {
   const { state } = useGameContext();
-  const { winner, isDraw } = state;
+  const { winner, isDraw, currentPlayer } = state;
 
   return (
     <div className="app__background">
       <h1>Connect 4</h1>
+      <p className="app__p">{`play ${currentPlayer}`}</p>
       <Board />
       <p className="app__p">{winner && `Player ${winner} wins!`}</p>
       <p className="app__p">{isDraw && `Draw!`}</p>
